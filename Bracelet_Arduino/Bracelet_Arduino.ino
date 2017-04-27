cf6c8133b4a6e47a8702b41fb30e82d1dcc0c865
Michael <MikeL1300@Gmail.com>
2017-04-28 00:36:10 +0300

    Changed the way tags are read using new library (need to install NDEF-master and remove NDEF 2), now reads tag messages instead of uid. Changed Tag_Data fields, now stores tag message instead of uid. Removed BT pairing.

--- Bracelet_Arduino/Bracelet_Arduino.ino
+++ Bracelet_Arduino/Bracelet_Arduino.ino
@@ -11,23 +11,27 @@
 #include "SPI.h"
 #include "PN532.h"
 #include "PN532_SPI.h"
+#include "NfcAdapter.h"
 
 #include "emulatetag.h"
 #include "NFC_Tags_Container.h"
-#include "Known_Tags_Container.h"
-#include "NFCPairingProtocol.h"
+//#include "Known_Tags_Container.h"
+//#include "NFCPairingProtocol.h"
 
 PN532_SPI pn532spi(SPI, 10);
-EmulateTag nfc(pn532spi);
-uint8_t emulatedUID[3] = { 0x12, 0x34, 0x56 };  //this is the UID of the emulated tag (that holds the message above for pairing)
-uint8_t bluetoothPairNFCMessage[] = BLUETOOTH_PAIR_NFC_MESSAGE;
+NfcAdapter nfc = NfcAdapter(pn532spi);
+
+//EmulateTag nfc(pn532spi);
+//uint8_t emulatedUID[3] = { 0x12, 0x34, 0x56 };  //this is the UID of the emulated tag (that holds the message above for pairing)
+//uint8_t bluetoothPairNFCMessage[] = BLUETOOTH_PAIR_NFC_MESSAGE;
 
 static constexpr int NFC_READ_TIMEOUT = 1000; //in ms.
 static constexpr int NFC_PAIR_TIMEOUT = 7000; //in ms.
 static constexpr int BT_DATA_SEND_TIMEOUT = 30000; //in ms.
+static constexpr int TAG_PRESENT_TIMEOUT = 500; //in ms
 
 NFC_Tags_Container tags_cont;
-Known_Tags_Container known_tags;
+//Known_Tags_Container known_tags;
 
 static constexpr int RX = 2; //On nano: white
 static constexpr int TX = 3; //On nano: grey
@@ -42,18 +46,15 @@ void setup(void) {
 	bluetoothSerial.begin(9600);
 	//bluetoothSerial.println(F("Starting"));
 
-	nfc.setNdefFile(bluetoothPairNFCMessage, sizeof(bluetoothPairNFCMessage));
+	nfc.begin();
+
+	/*nfc.setNdefFile(bluetoothPairNFCMessage, sizeof(bluetoothPairNFCMessage));
 	nfc.setUid(emulatedUID);
-	nfc.init();
-	uint32_t versiondata = nfc.getFirmwareVersion();
-	if (!versiondata) {
-		Serial.print(F("Didn't find PN53x board"));
-		while (1); // halt
-	}
+	nfc.init();*/
 }
 
 void loop(void) {
-	readTag(500);//0 = infinity timeout
+	readTag(TAG_PRESENT_TIMEOUT);//0 = infinity timeout
 
 	//checking if pairing is finished, and phone sent '#':
 	//in the future, we should add a flag that we are waiting for pair
@@ -68,27 +69,51 @@ void loop(void) {
 }
 
 void readTag(uint16_t timeout) {
-	//Serial.println(F("Waiting for an ISO14443A Card ..."));
+	if (nfc.tagPresent(timeout)) {
+		Serial.println(F("Found tag"));
+		NfcTag tag = nfc.read();
+		if (!tag.hasNdefMessage()) {
+			Serial.println(F("ERROR: No NDEF message!"));
+		}
+		else {
+			NdefMessage message = tag.getNdefMessage();
+			uint8_t recordCount = message.getRecordCount();
+			if (recordCount > 1) {
+				Serial.println(F("ERROR: More than 1 record!"));
+			}
+			else if (recordCount == 0) {
+				Serial.println(F("ERROR: No record!"));
+			}
+			NdefRecord record = message.getRecord(0); //we assume the message is in the 1st record.
+			uint8_t payloadLength = record.getPayloadLength();
+			byte payload[payloadLength];
+			record.getPayload(payload);
 
-	uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-	uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-	uint8_t success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, timeout);
-	if (!success) return;
-	if (known_tags.isKnownTag(uid)) {
-		Serial.print(F("Found known tag:"));
-		Serial.println();
-		nfc.PrintHex(uid, uidLength);
-		tags_cont.add(uid);
-		Serial.println(F("Memory Left:"));
-		Serial.println(freeMemory());
-		sendDataBT();
-		delay(NFC_READ_TIMEOUT);
-	}
-	else {
-		Serial.print(F("Found unknown tag:"));
-		Serial.println();
-		nfc.PrintHex(uid, uidLength);
-		tryToPairViaNFC(NFC_PAIR_TIMEOUT);
+			uint16_t message_data = atoi((char*)payload);
+			Serial.println(F("Message: "));
+			Serial.println(message_data);
+
+			tags_cont.add(type_tag_data, message_data);
+			sendDataBT();
+			Serial.println(F("Memory Left:"));
+			Serial.println(freeMemory());
+			Serial.println(F("DB size:"));
+			Serial.println(tags_cont.getSize());
+			
+			delay(NFC_READ_TIMEOUT);
+			
+			/*
+			// Can be used for debugging:
+			// Force the data into a String (might work depending on the content)
+			// Real code should use smarter processing
+			String payloadAsString = "";
+			for (int c = 0; c < payloadLength; c++) {
+				payloadAsString += (char)payload[c];
+			}
+			Serial.print("  Payload (as String): ");
+			Serial.println(payloadAsString);
+			*/
+		}
 	}
 }
 
@@ -100,26 +125,26 @@ void sendDataBT() {
 	bluetoothSerial.println('#'); //printing "ln" causes the phone app to crash!
 }
 
-void tryToPairViaNFC(uint16_t timeout) {
-	Serial.println(F("changing to pair state"));
-	nfc.init();//dont know why, but doesnt work without that here..
-	nfc.emulate(timeout);
-	
-	/* removed because we'll try dynamic pairing instead.
-	Serial.println(F("waiting for #"));
-	unsigned long temp = millis();
-	while (!bluetoothSerial.available()) { //waiting for some data to be sent from the phone to make sure there is a connection.
-		if (millis() - temp > BT_DATA_SEND_TIMEOUT) {
-			Serial.println(F("Timeout! Didnt get #!"));
-			break;
-		}
-	}
-	if (bluetoothSerial.available()) {
-		Serial.print(F("Got #"));
-	}
-	while (bluetoothSerial.available()) { //clearing the buffer.
-		bluetoothSerial.read();
-	}
-	sendDataBT();
-	*/
-}
+//void tryToPairViaNFC(uint16_t timeout) {
+//	Serial.println(F("changing to pair state"));
+//	nfc.init();//dont know why, but doesnt work without that here..
+//	nfc.emulate(timeout);
+//	
+//	/* removed because we'll try dynamic pairing instead.
+//	Serial.println(F("waiting for #"));
+//	unsigned long temp = millis();
+//	while (!bluetoothSerial.available()) { //waiting for some data to be sent from the phone to make sure there is a connection.
+//		if (millis() - temp > BT_DATA_SEND_TIMEOUT) {
+//			Serial.println(F("Timeout! Didnt get #!"));
+//			break;
+//		}
+//	}
+//	if (bluetoothSerial.available()) {
+//		Serial.print(F("Got #"));
+//	}
+//	while (bluetoothSerial.available()) { //clearing the buffer.
+//		bluetoothSerial.read();
+//	}
+//	sendDataBT();
+//	*/
+//}
--- Bracelet_Arduino/NFC_Tags_Container.h
+++ Bracelet_Arduino/NFC_Tags_Container.h
@@ -7,46 +7,62 @@
 #include <ArduinoSTL.h>
 #include <list>
 
+//bit field consts:
 static constexpr int TS_TIME_BITS = 11;
 static constexpr int TS_ID_BITS = 3;
+static constexpr int TYPE_BITS = 4;
+static constexpr int TAG_DATA_BITS = 16;
 
-struct Timestamp{ //This is a bit field!
+struct Timestamp{
 	unsigned int time : TS_TIME_BITS; //stored in one minute intervals.
 	unsigned int id : TS_ID_BITS; //in case several tags get scanned in the same minute.
+};
+
+enum Data_Type { type_tag_data }; //todo: add more data types.
+
+union Data
+{
+	unsigned int tagData : TAG_DATA_BITS; //type = type_tag_data
+	//todo: add more data types.
 };
 
 class Tag_Data {
 public:
 	Timestamp timestamp;
-	uint8_t uid[7];
-	Tag_Data(uint8_t uid[]){
+	Data_Type type : TYPE_BITS;
+	Data data;
+	Tag_Data(Data_Type type, uint32_t data) {
 		timestamp.time = millis() / 60000; //divided by number of ms in one minute.
 		timestamp.id = 0;
-		//uid = new uint8_t[7];
-		memcpy(this->uid, uid, 7);
+		this->type = type;
+		this->data.tagData = data;
 	}
-	/*
-	~Tag_Data() {
-		delete[] uid;
-	}
-	*/
+	// we dont save uid's anymore.
+	//uint8_t uid[7];
+	//Tag_Data(uint8_t uid[]){
+	//	timestamp.time = millis() / 60000; //divided by number of ms in one minute.
+	//	timestamp.id = 0;
+	//	//uid = new uint8_t[7];
+	//	memcpy(this->uid, uid, 7);
+	//}
 };
 
 class NFC_Tags_Container {
 private:
 	std::list<Tag_Data> tags;
 public:
-	void add(uint8_t uid[]) {
-		Tag_Data newTag = Tag_Data(uid);
+	void add(Data_Type type, uint32_t data) {
+		Tag_Data newTag = Tag_Data(type, data);
 		if (tags.size() > 0) {
 			const Tag_Data& last = tags.back();
-			if (last.timestamp.time == newTag.timestamp.time) {
+			if (last.timestamp.time >= newTag.timestamp.time) {
 				//if max number of tags (max val of timestamp.id) added this minute, we add to next minute
 				if (last.timestamp.id < pow(2, TS_ID_BITS) - 1) {
 					newTag.timestamp.id = last.timestamp.id + 1;
+					newTag.timestamp.time = last.timestamp.time;
 				}
 				else {
-					newTag.timestamp.time += 1;
+					newTag.timestamp.time = last.timestamp.time + 1;
 				}
 			}
 		}
@@ -57,22 +73,9 @@ public:
 		int count = tags.size();
 		int i = 1;
 		for (auto td : tags) {
-			stream.print(F("{\"uid\":\""));
-			char hexid[21] = { 0 };
-			for (int i = 0; i < 7; i++) {
-				if (td.uid[i] <= 0x0F) {
-					itoa(td.uid[i], &hexid[i * 3 + 1], 16);
-					hexid[i * 3] = '0';
-				}
-				else if (td.uid[i] <= 0xFF) {
-					itoa(td.uid[i], &hexid[i * 3], 16);
-				} //else error.. cant be coz td.[uid] is a byte
-				hexid[i * 3 + 2] = ' ';
-
-			}
-			hexid[20] = 0;
-			stream.print(hexid);
-
+			//printUID(stream, td);
+			stream.print(F("{\"data\":\""));
+			stream.print(td.data.tagData);
 			stream.print(F("\",\"ts\":\""));
 			stream.print(td.timestamp.time);
 			stream.print(F("\",\"tsid\":\""));
@@ -83,8 +86,29 @@ public:
 		}
 		stream.print("]");
 	}
+	int getSize() {
+		return tags.size();
+	}
 };
 
-
+//moved the old uid print method to a function in case we need it in the future.
+//void printUID(Stream & stream, Tag_Data &td)
+//{
+//	stream.print(F("{\"uid\":\""));
+//	char hexid[21] = { 0 };
+//	for (int i = 0; i < 7; i++) {
+//		if (td.uid[i] <= 0x0F) {
+//			itoa(td.uid[i], &hexid[i * 3 + 1], 16);
+//			hexid[i * 3] = '0';
+//		}
+//		else if (td.uid[i] <= 0xFF) {
+//			itoa(td.uid[i], &hexid[i * 3], 16);
+//		} //else error.. cant be coz td.[uid] is a byte
+//		hexid[i * 3 + 2] = ' ';
+//
+//	}
+//	hexid[20] = 0;
+//	stream.print(hexid);
+//}
 
 #endif
